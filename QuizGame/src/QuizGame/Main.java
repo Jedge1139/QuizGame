fd17beedf1812d141a71f5c18ec804df6f59fca2
pirat <pirat@DESKTOP-SMUMFPH>
2021-05-22 12:13:49 -0400

    Initial Commit

---
+++ QuizGame/src/QuizGame/Main.java
@@ -0,0 +1,23 @@
+package QuizGame;
+//Jacob Edge
+//A Quiz game about programming and items that I have learned in COP 2006.
+/*Byte-
+*Short-
+*Int-
+*Long-
+*Float-
+*Double-
+*Boolean-
+*Char-
+*/
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
